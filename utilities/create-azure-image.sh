be22d4624c15acc09aafc3c3f57bfcd6f651e960
Ming Ho <ming.ho@couchbase.com>
2023-08-23 11:40:26 -0700

    AV-53316, expand azure to switzerlandnorth.
    -Ming
    
    Change-Id: I6ed75d89817f65e09f7f10b8e5c0c8f9fada3b92
    Reviewed-on: https://review.couchbase.org/c/cloud-build-tools/+/195921
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- utilities/create-azure-image.sh
+++ utilities/create-azure-image.sh
@@ -108,7 +108,7 @@ RESOURCE_GROUP="image-factory"
 GALLERY_NAME="capella"
 PLATFORM="linux"
 REGION="eastus"
-REPLICATION_REGIONS='["australiaeast", "brazilsouth", "centralindia", "centralus", "canadacentral", "eastasia", "eastus", "eastus2", "francecentral", "germanywestcentral", "swedencentral", "japaneast", "koreacentral", "northeurope", "norwayeast", "southeastasia", "southcentralus", "uksouth", "westeurope", "westus2", "westus3"]'
+REPLICATION_REGIONS='["australiaeast", "brazilsouth", "centralindia", "centralus", "canadacentral", "eastasia", "eastus", "eastus2", "francecentral", "germanywestcentral", "swedencentral", "japaneast", "koreacentral", "northeurope", "norwayeast", "southeastasia", "southcentralus", "switzerlandnorth", "uksouth", "westeurope", "westus2", "westus3"]'
 
 while getopts p:r:v:b:c:g:s:i:t:o:w: opt
 do
