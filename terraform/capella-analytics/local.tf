06734e0d31d24d982bcbf2d9921a8e7bfe1155e2
Ming Ho <ming.ho@couchbase.com>
2023-08-29 18:48:54 -0700

    CBD-5578, terraform scripts to create ecr for capella analytics.
    -Ming
    
    Change-Id: I3c7fe0b24a9bf3c3694a05c62dd342a3e33a33a8
    Reviewed-on: https://review.couchbase.org/c/cloud-build-tools/+/196342
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

---
+++ terraform/capella-analytics/local.tf
@@ -0,0 +1,30 @@
+data "aws_caller_identity" "current" {}
+
+# aws accounts:
+#   264138468394: Capella sbx env (dbaas5)
+#   516524556673: cb-qe
+#   955582452726: cb-perf
+#   768250113943: AWS dev
+
+
+locals {
+  repositories = ["capella-analytics", "goldfish-nebula"]
+  aws_region             = "us-east-2"
+  ecr_image_scan         = true
+  couchbase_ips          = [
+    "12.145.26.240/29",
+    "12.235.169.89/30",
+    "32.142.206.162/30",
+    "64.124.71.194/29",
+    "67.212.150.204/27"
+  ]
+  repo_pull_access_arns  = [
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+    "arn:aws:iam::264138468394:root",
+    "arn:aws:iam::516524556673:root",
+    "arn:aws:iam::768250113943:root",
+    "arn:aws:iam::955582452726:root"
+  ]
+  repo_push_access_arns  = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+  repo_push_access_roles = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/cbd-4108_server_jenkins_worker"]
+}
