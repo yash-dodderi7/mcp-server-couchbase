7865d88319df1002537e1fffa02bc8102d961f2d
Ming Ho <ming.ho@couchbase.com>
2023-09-26 09:15:28 -0700

    CBD-5578, allow ECR images to be retagged. This is necessary for tags such as latest.
    -Ming
    
    Change-Id: I315d69bc456257e6b1acca585c7d8c8a53bb165d
    Reviewed-on: https://review.couchbase.org/c/cloud-build-tools/+/197760
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- terraform/capella-analytics/modules/ecr/aws_ecr_repository.tf
+++ terraform/capella-analytics/modules/ecr/aws_ecr_repository.tf
@@ -5,7 +5,7 @@
 #   scan via open-source Clair project
 resource "aws_ecr_repository" "aws_ecr_repo" {
   name                 = "${var.ecr_repo_name}"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = var.ecr_image_scan
   }
