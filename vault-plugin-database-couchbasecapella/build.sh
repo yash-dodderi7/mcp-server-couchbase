e8c394e996e42b5909db68c5b781c0dbe16bd8db
Ming Ho <ming.ho@couchbase.com>
2023-11-21 10:53:04 -0800

    AV-64822, move terraform-provider and vault-plugin-database-couchbasecapella build and publishing scripts to repo.
    -Ming
    
    Change-Id: I120dd6a51adb1791a6f0add60243a04535e676c3
    Reviewed-on: https://review.couchbase.org/c/cloud-build-tools/+/201246
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

---
+++ vault-plugin-database-couchbasecapella/build.sh
@@ -0,0 +1,94 @@
+#!/bin/bash -ex
+
+# Simple script for building vault-plugin-database-couchbasecapella
+
+function usage() {
+  echo
+  echo "$0 -p <product> -v <version> -b <build-number>"
+  echo "where:"
+  echo "  -p: product"
+  echo "  -v: version number"
+  echo "  -b: build number"
+  echo
+}
+
+function install_deps() {
+  SERVER_ARCH=$(uname -m)
+  curl -L https://packages.couchbase.com/cbdep/cbdep-linux-${SERVER_ARCH} -o cbdep
+  chmod +x cbdep
+  GO_MIN_VER=$(cat go.mod | grep ^go |awk '{print $2}')
+  GO_VER=$(echo -e "1.21.3\n${GO_MIN_VER}" |sort -r | head -1)
+  ./cbdep install golang ${GO_VER} -d extra
+  export PATH=`pwd`/extra/go${GO_VER}/bin:$PATH
+}
+
+function build() {
+  export TF_CLI_CONFIG_FILE=${WORKSPACE}/.terraformrc
+cat <<EOT >> ${TF_CLI_CONFIG_FILE}
+provider_installation {
+dev_overrides {
+"hashicorp.com/couchabasecloud/capella" = "${WORKSPACE}/extra/go${GO_VER}/bin/"
+}
+direct {}
+}
+EOT
+
+  OSTYPES="linux darwin windows"
+  for OSTYPE in ${OSTYPES}; do
+    case "${OSTYPE}" in
+      linux)
+        for ARCH in amd64 arm64; do
+          GOOS=${OSTYPE} GOARCH=${ARCH} go build \
+            -o couchbasecapella-database-plugin ./cmd/couchbasecapella-database-plugin/. 
+          zip ${PRODUCT}-${VERSION}-${BLD_NUM}_${OSTYPE}_${ARCH}.zip \
+            couchbasecapella-database-plugin
+        done
+        GOARM=6 GOARCH=arm GOOS=${OSTYPE} go build \
+          -o couchbasecapella-database-plugin ./cmd/couchbasecapella-database-plugin/. 
+        zip ${PRODUCT}-${VERSION}-${BLD_NUM}_${OSTYPE}_${ARCH}.zip \
+          couchbasecapella-database-plugin
+        ;;
+      darwin)
+        for ARCH in amd64 arm64; do
+          GOOS=${OSTYPE} GOARCH=${ARCH} go build \
+            -o couchbasecapella-database-plugin ./cmd/couchbasecapella-database-plugin/. 
+          zip ${PRODUCT}-${VERSION}-${BLD_NUM}_${OSTYPE}_${ARCH}.zip \
+            couchbasecapella-database-plugin
+        done
+        ;;
+      windows)
+        ARCH=amd64
+        GOOS=${OSTYPE} GOARCH=${ARCH} go build \
+          -o couchbasecapella-database-plugin.exe ./cmd/couchbasecapella-database-plugin/. 
+        zip ${PRODUCT}-${VERSION}-${BLD_NUM}_${OSTYPE}_${ARCH}.zip \
+          couchbasecapella-database-plugin.exe
+        ;;
+      *)
+        echo "unknown: $OSTYPE"
+        exit 1
+        ;;
+    esac
+  done
+}
+
+## main
+while getopts "b:p:v:" opt; do
+  case $opt in
+    b) BLD_NUM=$OPTARG;;
+    p) PRODUCT=$OPTARG;;
+    v) VERSION=$OPTARG;;
+    *) echo "Invalid argument $opt"
+      usage
+      exit 1;;
+  esac
+done
+
+if [[ -z ${PRODUCT} || -z ${VERSION} || -z ${BLD_NUM} ]]; then
+    usage
+    exit 1
+fi
+
+pushd ${WORKSPACE}/${PRODUCT}
+install_deps
+build
+popd
---
+++ vault-plugin-database-couchbasecapella/publish.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -ex
+
+function usage() {
+  echo
+  echo "$0 -p <product> -v <version> -b <build-number>"
+  echo "where:"
+  echo "  -p: product"
+  echo "  -r: release"
+  echo "  -v: version number"
+  echo "  -b: build number"
+  echo
+}
+
+function gh_auth() {
+  # github_token is a predefined variable either from jenkins or from command line
+  echo "${github_token}" | gh auth login --with-token
+}
+
+function publish() {
+  SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+  ${SCRIPT_DIR}/../utilities/sync_historic_manifest \
+    ${PRODUCT} ${RELEASE} ${VERSION} ${BLD_NUM} ./src
+
+  pushd src/${PRODUCT}
+  SHA=$(git rev-list HEAD | head -1)
+  LATESTBUILD_URL="https://latestbuilds.service.couchbase.com/builds/latestbuilds"
+  for plat in darwin_amd64 darwin_arm64 linux_amd64 linux_arm64 windows_amd64; do
+    src_file=${PRODUCT}-${VERSION}-${BLD_NUM}_${plat}.zip
+    tgt_file=${PRODUCT}-${VERSION}_${plat}.zip
+    curl --fail ${LATESTBUILD_URL}/${PRODUCT}/${VERSION}/${BLD_NUM}/${src_file} \
+      -o ${tgt_file}
+    sha256sum ${tgt_file} >> ${PRODUCT}-${VERSION}_checksums.txt
+  done
+
+  files=$(ls *.zip *_checksums.txt)
+  gh release create v${VERSION} ${files} --generate-notes --target ${SHA}
+  popd
+}
+
+## main
+while getopts "b:p:r:v:" opt; do
+  case $opt in
+    b) BLD_NUM=$OPTARG;;
+    p) PRODUCT=$OPTARG;;
+    r) RELEASE=$OPTARG;;
+    v) VERSION=$OPTARG;;
+    *) echo "Invalid argument $opt"
+      usage
+      exit 1;;
+  esac
+done
+
+if [[ -z ${PRODUCT} || -z ${VERSION} || -z ${RELEASE} || -z ${BLD_NUM} ]]; then
+    usage
+    exit 1
+fi
+
+gh_auth
+publish
