ba1ef986ec400db6f1c84027ee8704e34b1d8909
Ming Ho <ming.ho@couchbase.com>
2025-09-11 14:51:59 -0700

    AV-110393, add dp-observer to dp-runtime-agent AMI.
    add dp-observer along with node-exporter and process-exporter.
    
    -Ming
    
    Change-Id: I3d0740dfd0804df39d2b376045ae90dee5789836
    Reviewed-on: https://review.couchbase.org/c/cloud-build-tools/+/233624
    Reviewed-by: Chris Farman <christopher.farman@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Ayan Sharma

--- dp-runtime-agent/aws/dp-runtime-agent.pkr.hcl
+++ dp-runtime-agent/aws/dp-runtime-agent.pkr.hcl
@@ -18,6 +18,10 @@ locals {
   ami_name        = var.ami_name != "" ? var.ami_name : "${local.product_name}-${var.product_version}-${local.product_arch}"
   source_ami_name = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server*"
   instance_type   = "t2.micro"
+  process-exporter_version = "0.8.7"
+  process-exporter_package = "process-exporter_${local.process-exporter_version}_linux_${local.product_arch}"
+  node_exporter_version    = "1.9.1"
+  node_exporter_package    = "node_exporter-${local.node_exporter_version}.linux-${local.product_arch}"
 }
 
 source "amazon-ebs" "cc" {
@@ -53,13 +57,24 @@ build {
     destination = "/tmp/"
     sources     = [
       "agents/${local.product_arch}/${local.product_name}.gz",
+      "agents/${local.product_arch}/dp-observer.gz",
       "${local.product_name}.service",
+      "dp-observer.service",
+      "node-exporter.service",
+      "process-exporter.service",
       "journald.conf",
       "provision.sh"
     ]
   }
   provisioner "shell" {
+    environment_vars = [
+      "NODE_EXPORTER_VERSION=${local.node_exporter_version}",
+      "NODE_EXPORTER_PACKAGE=${local.node_exporter_package}",
+      "PROCESS_EXPORTER_VERSION=${local.process-exporter_version}",
+      "PROCESS_EXPORTER_PACKAGE=${local.process-exporter_package}"
+    ]
     pause_before = "5s"
+    execute_command = "sudo -E sh -x -c '{{ .Vars }} {{ .Path }}'"
     script = "provision.sh"
   }
 }
--- dp-runtime-agent/aws/provision.sh
+++ dp-runtime-agent/aws/provision.sh
@@ -2,18 +2,43 @@
 
 # run unattended-upgrade to apply kernel and security patches
 # then disable it so that it so that it doesn't cause unexpected side effect in production
-sudo apt update
-sudo unattended-upgrade
-sudo systemctl disable --now unattended-upgrades.service
-sudo sed -i 's/^APT::Periodic::Unattended-Upgrade\s*"\?1"\?;/APT::Periodic::Unattended-Upgrade "0";/' /etc/apt/apt.conf.d/20auto-upgrades
+apt update
+unattended-upgrade
+systemctl disable --now unattended-upgrades.service
+sed -i 's/^APT::Periodic::Unattended-Upgrade\s*"\?1"\?;/APT::Periodic::Unattended-Upgrade "0";/' /etc/apt/apt.conf.d/20auto-upgrades
 
 
-sudo mv /tmp/journald.conf /etc/systemd/journald.conf
-sudo chown root:root /etc/systemd/journald.conf
-sudo chmod 755 /etc/systemd/journald.conf
+mv /tmp/journald.conf /etc/systemd/journald.conf
+chown root:root /etc/systemd/journald.conf
+chmod 755 /etc/systemd/journald.conf
+
+# Create couchbase user
+# dp-observer depends on this
+useradd couchbase && sudo usermod -a -G systemd-journal couchbase && sudo usermod -a -G couchbase ubuntu
+
+# Install and start node exporter
+wget "https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/${NODE_EXPORTER_PACKAGE}.tar.gz" -P /tmp/
+tar xvfz /tmp/"${NODE_EXPORTER_PACKAGE}".tar.gz -C /home/ubuntu/ --strip-components=1 "${NODE_EXPORTER_PACKAGE}"/node_exporter
+chown ubuntu:ubuntu /home/ubuntu/node_exporter
+mv /tmp/node-exporter.service /lib/systemd/system/node-exporter.service
+systemctl enable node-exporter.service
+
+# Install and enable process exporter
+wget "https://github.com/ncabatoff/process-exporter/releases/download/v${PROCESS_EXPORTER_VERSION}/${PROCESS_EXPORTER_PACKAGE}.deb" -P /tmp/
+apt install -y /tmp/"${PROCESS_EXPORTER_PACKAGE}".deb
+mv /tmp/process-exporter.service /lib/systemd/system/process-exporter.service
+systemctl enable process-exporter.service
 
 # Install and enable dp-runtime-agent
 gunzip -c /tmp/dp-runtime-agent.gz > /home/ubuntu/dp-runtime-agent
+chown ubuntu:ubuntu /home/ubuntu/dp-runtime-agent
 chmod +x /home/ubuntu/dp-runtime-agent
-sudo mv /tmp/dp-runtime-agent.service /lib/systemd/system/dp-runtime-agent.service
-sudo systemctl enable dp-runtime-agent.service
+mv /tmp/dp-runtime-agent.service /lib/systemd/system/dp-runtime-agent.service
+systemctl enable dp-runtime-agent.service
+
+# install & enable dp-observer
+mv /tmp/dp-observer.service /lib/systemd/system/dp-observer.service
+mv /tmp/dp-observer.gz /home/ubuntu && gunzip /home/ubuntu/dp-observer.gz
+chmod +x /home/ubuntu/dp-observer
+chown ubuntu:ubuntu /home/ubuntu/dp-observer
+systemctl enable dp-observer.service
